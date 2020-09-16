948e7226db24c13049586a3b930207de3a08d1ba
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2020-09-16 10:43:09 +0200

    Amélioration de l'extraction des termes de recherche

--- ophirofox/content_scripts/europresse_search.js
+++ ophirofox/content_scripts/europresse_search.js
@@ -3,9 +3,10 @@ function onLoad() {
     const source = url.searchParams.get("ophirofox_source");
     if (!source) return;
     const source_url = new URL(source);
-    const lemonde_match = source_url.pathname.match(/([^/.]+)(\.html)?$/);
+    const lemonde_match = source_url.pathname.match(/([^/.]+)(_\d*_\d*\.html)?$/);
     if (!lemonde_match) throw new Error("Could not find keywords in lemonde url");
-    const search_terms = lemonde_match[1].split(/[^a-z]+/).join(" ");
+    const stopwords = new Set(['d', 'l', 'et'])
+    const search_terms = lemonde_match[1].split('-').filter(w => !stopwords.has(w)).join(" ");
     const keyword_field = document.getElementById("Keywords");
     keyword_field.value = search_terms;
     keyword_field.form.submit();
