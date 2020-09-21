4619db73e06af58ec3c39caae31af6a8ebcdac2a
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2020-09-21 16:34:07 +0200

    Extraction des mots clefs directement depuis lemonde

--- ophirofox/content_scripts/europresse_search.js
+++ ophirofox/content_scripts/europresse_search.js
@@ -1,12 +1,7 @@
 function onLoad() {
     const url = new URL(window.location);
-    const source = url.searchParams.get("ophirofox_source");
-    if (!source) return;
-    const source_url = new URL(source);
-    const lemonde_match = source_url.pathname.match(/([^/.]+)(_\d*_\d*\.html)?$/);
-    if (!lemonde_match) throw new Error("Could not find keywords in lemonde url");
-    const stopwords = new Set(['d', 'l', 'et'])
-    const search_terms = lemonde_match[1].split('-').filter(w => !stopwords.has(w)).join(" ");
+    const search_terms = url.searchParams.get("ophirofox_keywords");
+    if (!search_terms) return;
     const keyword_field = document.getElementById("Keywords");
     keyword_field.value = search_terms;
     keyword_field.form.submit();
--- ophirofox/content_scripts/lemonde.js
+++ ophirofox/content_scripts/lemonde.js
@@ -4,9 +4,19 @@ function makeEuropresseUrl(lemondeUrl) {
     const target_url = new URL("https://nouveau.europresse.com/Search/Reading");
     //target_url.searchParams.set("docKey", `news·${m[1]}${m[2]}${m[3]}·LMF·${m[4]}`);
     target_url.searchParams.set("ophirofox_source", window.location);
+    target_url.searchParams.set("ophirofox_keywords", extractKeywords(window.location));
     //target_url.hash = "docText";
     const url = new URL("http://proxy.rubens.ens.fr/login?url=" + target_url);
     return url;
+}
+
+function extractKeywords(url) {
+    const source_url = new URL(url);
+    const lemonde_match = source_url.pathname.match(/([^/.]+)(_\d*_\d*\.html)?$/);
+    if (!lemonde_match) throw new Error("Could not find keywords in lemonde url");
+    const stopwords = new Set(['d', 'l', 'et'])
+    const search_terms = lemonde_match[1].split('-').filter(w => !stopwords.has(w));
+    return search_terms.join(" ");
 }
 
 function createLink() {
