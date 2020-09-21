0b3ebb9bef5c1687eb1d0015c107755174641e23
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2020-09-21 19:05:00 +0200

    Ajout du site du figaro

---
+++ ophirofox/content_scripts/lefigaro.js
@@ -0,0 +1,35 @@
+function makeEuropresseUrl(lemondeUrl) {
+    const target_url = new URL("https://nouveau.europresse.com/Search/Reading");
+    target_url.searchParams.set("ophirofox_source", window.location);
+    target_url.searchParams.set("ophirofox_keywords", extractKeywords());
+    const url = new URL("http://proxy.rubens.ens.fr/login?url=" + target_url);
+    return url;
+}
+
+function extractKeywords() {
+    return document.querySelector("h1").textContent.split(/[^\w]+/).join(" ");
+}
+
+function createLink() {
+    const a = document.createElement("a");
+    a.href = makeEuropresseUrl(new URL(window.location));
+    a.textContent = "Lire sur Europresse";
+    a.className = "ophirofox-europresse";
+    return a;
+}
+
+
+function findPremiumBanner() {
+    const title = document.querySelector("h1");
+    if (!title) return null;
+    const divs = title.parentElement.querySelectorAll("div");
+    return [...divs].find(d => d.textContent.includes("Réservé aux abonnés"))
+}
+
+function onLoad() {
+    const premiumBanner = findPremiumBanner();
+    if (!premiumBanner) return;
+    premiumBanner.after(createLink());
+}
+
+onLoad();
