d4d1ab6ae227450642fdd08125b4434de8689db7
pierrevedel <49493876+pierrevedel@users.noreply.github.com>
2021-01-29 20:20:23 +0100

    switch from ENS to ENSAM auth adresss

--- ophirofox/content_scripts/europresse_view.js
+++ ophirofox/content_scripts/europresse_view.js
@@ -2,5 +2,6 @@ if (document.body.textContent.match("Désolé ! Une erreur est survenue")) {
     const ophirofox_target = window.location;
     //const service = `https://nouveau-europresse-com.proxy.rubens.ens.fr/Search/Reading?ophirofox_target=${encodeURIComponent(ophirofox_target)}`;
     const service = `https://nouveau-europresse-com.rp1.ensam.eu/Search/Reading?ophirofox_target=${encodeURIComponent(ophirofox_target)}`;
-    window.location = "https://sso.ens.fr/cas/login?service=" + encodeURIComponent(service);
+    //window.location = "https://sso.ens.fr/cas/login?service=" + encodeURIComponent(service);
+    window.location = "https://auth.ensam.eu/cas/login?service=" + encodeURIComponent(service);
 }
