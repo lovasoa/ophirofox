fb98b27ed32fa0fdf60073ecddf3dc168c1197fa
Jules Sanglier <jules.sanglier@gmail.com>
2022-02-25 15:54:08 +0100

    support Monde Diplomatique

---
+++ ophirofox/content_scripts/monde-diplo.js
@@ -0,0 +1,38 @@
+async function makeEuropresseUrl(lemondeUrl) {
+    const keywords = extractKeywords();
+    return await makeOphirofoxReadingLink(keywords);
+}
+
+function extractKeywords() {
+    return extractKeywordsFromTitle();
+}
+
+function extractArticleNumber(){
+    let url = window.location.pathname;
+    let parameters = url.split('/');
+    return parameters[parameters.length - 1];
+}
+
+function extractKeywordsFromTitle() {
+    const titleElem = document.getElementsByClassName("crayon article-titre-" + extractArticleNumber() + " h1")[0].innerHTML;
+    return titleElem;
+}
+
+async function createLink() {
+    const a = document.createElement("a");
+    a.textContent = "Lire sur Europresse";
+    a.className = "btn btn--premium ophirofox-europresse";
+    a.href = await makeEuropresseUrl(new URL(window.location));
+    return a;
+}
+
+async function onLoad() {
+    const payArticle = document.querySelector('.promo_dispo_article');
+    if (!payArticle) return;
+
+    const actionElem = document.querySelector(".actions-article");
+    if (!actionElem) return;
+    actionElem.appendChild(await createLink());
+}
+
+onLoad().catch(console.error);
