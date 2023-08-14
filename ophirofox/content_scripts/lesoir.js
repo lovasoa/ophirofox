f2e44b78cc492c2fd9d2583f199406073ed9ab90
fmoncomble <59739627+fmoncomble@users.noreply.github.com>
2023-08-14 20:59:23 +0200

    Create lesoir.js

---
+++ ophirofox/content_scripts/lesoir.js
@@ -0,0 +1,32 @@
+async function makeEuropresseUrl() {
+    const keywords = extractKeywords();
+    return await makeOphirofoxReadingLink(keywords);
+}
+
+function extractKeywords() {
+    return document.querySelector("h1").textContent;
+}
+
+async function createLink() {
+    const a = document.createElement("a");
+    a.href = await makeEuropresseUrl(new URL(window.location));
+    a.textContent = "Lire sur Europresse";
+    a.className = "fig-premium-mark-article__text ophirofox-europresse";
+    return a;
+}
+
+
+function findPremiumBanner() {
+    const title = document.querySelector("h1");
+    if (!title) return null;
+    const elems = title.parentElement.querySelectorAll("span");
+    return [...elems].find(d => d.textContent.includes("réservé aux abonnés"))
+}
+
+async function onLoad() {
+    const premiumBanner = findPremiumBanner();
+    if (!premiumBanner) return;
+    premiumBanner.after(await createLink());
+}
+
+onLoad().catch(console.error);
