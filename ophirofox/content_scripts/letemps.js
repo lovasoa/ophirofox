a4e79118ad184f6ffe2d6dae7649ec22be99ae5e
fmoncomble <59739627+fmoncomble@users.noreply.github.com>
2023-08-16 00:15:39 +0200

    Update letemps.js

--- ophirofox/content_scripts/letemps.js
+++ ophirofox/content_scripts/letemps.js
@@ -1,20 +1,12 @@
-async function makeEuropresseUrl() {
-    const keywords = extractKeywords();
-    return await makeOphirofoxReadingLink(keywords);
-}
-
 function extractKeywords() {
     return document.querySelector("h1").textContent;
 }
 
 async function createLink() {
-    const a = document.createElement("a");
-    a.href = await makeEuropresseUrl(new URL(window.location));
-    a.textContent = "Lire sur Europresse";
-    a.className = "post-premium is-long ophirofox-europresse";
+    const a = await ophirofoxEuropresseLink(extractKeywords());
+    a.classList.add("post-premium is-long");
     return a;
 }
-
 
 function findPremiumBanner() {
     const title = document.querySelector("article");
