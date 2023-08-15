d27e75616617b3eba9b27e378c5010721b90f89d
fmoncomble <59739627+fmoncomble@users.noreply.github.com>
2023-08-15 09:29:45 +0200

    Update ft.js

--- ophirofox/content_scripts/ft.js
+++ ophirofox/content_scripts/ft.js
@@ -1,32 +1,39 @@
-async function makeEuropresseUrl() {
+async function makeEuropresseUrl(ftUrl) {
+    const m = ftUrl.pathname.match(/(\d{4})\/(\d{2})\/(\d{2})\/.*?(\d+_\d+).html/);
+    if (!m) throw new Error("Format d'URL ft inconnu");
+    // docKey : `news·${m[1]}${m[2]}${m[3]}·LMF·${m[4]}`);
     const keywords = extractKeywords();
     return await makeOphirofoxReadingLink(keywords);
 }
 
 function extractKeywords() {
-    return document.querySelector("h1").textContent;
+    return extractKeywordsFromTitle() || extractKeywordsFromUrl(window.location);
+}
+
+function extractKeywordsFromTitle() {
+    const titleElem = document.querySelector("h1.article__title");
+    return titleElem && titleElem.textContent;
+}
+
+function extractKeywordsFromUrl(url) {
+    const source_url = new URL(url);
+    const ft_match = source_url.pathname.match(/([^/.]+)(_\d*_\d*\.html)?$/);
+    if (!ft_match) throw new Error("Could not find keywords in ft url");
+    return ft_match[1];
 }
 
 async function createLink() {
     const a = document.createElement("a");
-    a.href = await makeEuropresseUrl(new URL(window.location));
     a.textContent = "Lire sur Europresse";
-    a.className = "fig-premium-mark-article__text ophirofox-europresse";
+    a.className = "btn btn--premium ophirofox-europresse";
+    a.href = await makeEuropresseUrl(new URL(window.location));
     return a;
 }
 
-
-function findPremiumBanner() {
-    const title = document.querySelector("h1");
-    if (!title) return null;
-    const elems = title.parentElement.querySelectorAll("span");
-    return [...elems].find(d => d.textContent.includes("Keep reading"))
-}
-
 async function onLoad() {
-    const premiumBanner = findPremiumBanner();
-    if (!premiumBanner) return;
-    premiumBanner.after(await createLink());
+    const statusElem = document.querySelector(".article__status");
+    if (!statusElem) return;
+    statusElem.appendChild(await createLink());
 }
 
 onLoad().catch(console.error);
