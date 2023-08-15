eec05f209326c34ed6cc8d5190f3460d1193f912
fmoncomble <59739627+fmoncomble@users.noreply.github.com>
2023-08-16 00:13:00 +0200

    Update lalibre.js

--- ophirofox/content_scripts/lalibre.js
+++ ophirofox/content_scripts/lalibre.js
@@ -1,20 +1,12 @@
-async function makeEuropresseUrl() {
-    const keywords = extractKeywords();
-    return await makeOphirofoxReadingLink(keywords);
-}
-
 function extractKeywords() {
     return document.querySelector("h1").textContent;
 }
 
 async function createLink() {
-    const a = document.createElement("a");
-    a.href = await makeEuropresseUrl(new URL(window.location));
-    a.textContent = "Lire sur Europresse";
-    a.className = "ap-StoryDate-update ophirofox-europresse";
+    const a = await ophirofoxEuropresseLink(extractKeywords());
+    a.classList.add("ap-StoryDate-update");
     return a;
 }
-
 
 async function onLoad() {
     const statusElem = document.getElementsByClassName("ap-PaidPicto");
