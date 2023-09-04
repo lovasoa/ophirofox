d95f9793adaa06876f037d7b402198f2ee593173
fmoncomble <florent.moncomble@gmail.com>
2023-09-04 14:49:07 +0200

    Ajout L'Obs

---
+++ ophirofox/content_scripts/lobs.js
@@ -0,0 +1,15 @@
+const title = document.querySelector(".article-page-header");
+    
+function findPremiumBanner() {
+    if (!title) return null;
+    const elems = title.parentElement.querySelectorAll("a");
+    return [...elems].find(d => d.href.includes("ph-abo"))
+}
+
+async function onLoad() {
+    const head = findPremiumBanner();
+    if (!head) return;
+    head.after(await ophirofoxEuropresseLink());
+}
+
+onLoad().catch(console.error);
