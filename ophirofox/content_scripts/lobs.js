6d2ea8ccffc4c4a3c85a18e3b218ebf7c76c3c9e
fmoncomble <59739627+fmoncomble@users.noreply.github.com>
2023-09-04 15:05:04 +0200

    Update ophirofox/content_scripts/lobs.js
    Co-authored-by: Ophir LOJKINE <contact@ophir.dev>

--- ophirofox/content_scripts/lobs.js
+++ ophirofox/content_scripts/lobs.js
@@ -1,6 +1,5 @@
-const title = document.querySelector(".article-page-header");
-    
 function findPremiumBanner() {
+    const title = document.querySelector(".article-page-header");
     if (!title) return null;
     const elems = title.parentElement.querySelectorAll("a");
     return [...elems].find(d => d.href.includes("ph-abo"))
