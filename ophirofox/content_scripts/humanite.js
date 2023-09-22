5b60dc28c2cbc7d0a00fabd4648732424589667f
lovasoa <pere.jobs@gmail.com>
2023-09-22 08:46:39 +0200

    adaptation au nouveau site de l'huma

--- ophirofox/content_scripts/humanite.js
+++ ophirofox/content_scripts/humanite.js
@@ -1,20 +1,16 @@
 async function createLink() {
     const a = await ophirofoxEuropresseLink();
-
-    const div = document.createElement("div");
-    div.className = "field-name-field-news-auteur ophirofox-europresse";
-    div.appendChild(a);
-	
-    return div;
+    a.className = "rubric t-header-small c-red ophirofox-europresse";
+    return a;
 }
 
 async function onLoad() {
-    const reserve = document.querySelector(".qiota_reserve");
+    const reserve = document.querySelector("main .single__categories .u-icon-32");
     if (!reserve) return;
-	
-    const auteurElem = document.querySelector(".group-ft-auteur-date-media");
+
+    const auteurElem = document.querySelector(".single__content__text__published");
     if (!auteurElem) return;
-	
+
     auteurElem.appendChild(await createLink());
 }
 
