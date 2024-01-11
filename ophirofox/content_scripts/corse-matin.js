6221f9b43c906362347b13edf929e917d85bad7e
fmoncomble <florent.moncomble@gmail.com>
2024-01-11 18:56:59 +0100

    Ajout de Corse Matin, La Tribune, L'Opinion, Nice-Matin

---
+++ ophirofox/content_scripts/corse-matin.js
@@ -0,0 +1,20 @@
+async function createLink() {
+    const a = await ophirofoxEuropresseLink();
+    a.classList.add();
+    return a;
+}
+
+
+function findPremiumBanner() {
+    const title = document.querySelector("div.ml-2");
+    if (!title) return null;
+    return title;
+}
+
+async function onLoad() {
+	const premiumBanner = findPremiumBanner();
+    if (!premiumBanner) return;
+    premiumBanner.after(await createLink());
+}
+
+onLoad().catch(console.error);
---
+++ ophirofox/content_scripts/nice-matin.js
@@ -0,0 +1,20 @@
+async function createLink() {
+    const a = await ophirofoxEuropresseLink();
+    a.classList.add();
+    return a;
+}
+
+
+function findPremiumBanner() {
+    const title = document.querySelector("div.header-subscriber");
+    if (!title) return null;
+    return title;
+}
+
+async function onLoad() {
+	const premiumBanner = findPremiumBanner();
+    if (!premiumBanner) return;
+    premiumBanner.appendChild(await createLink());
+}
+
+onLoad().catch(console.error);
