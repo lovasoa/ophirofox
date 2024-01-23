cb55c021f4e946253e28640c83655f1da5b30730
lbaudin <119847138+lbaudin@users.noreply.github.com>
2024-01-23 10:32:51 +0100

    Add files via upload
    Lire sur europresse

---
+++ ophirofox/content_scripts/lesechos.js
@@ -0,0 +1,33 @@
+function extractKeywords() {
+    return extractKeywordsFromTitle() || extractKeywordsFromUrl(window.location);
+}
+
+function extractKeywordsFromTitle() {
+    const titleElem = document.querySelector("h1").childNodes[0];
+    return titleElem && titleElem.textContent;
+}
+
+function extractKeywordsFromUrl(url) {
+    /*const source_url = new URL(url);
+    const lemonde_match = source_url.pathname.match(/([^/.]+)(_\d*_\d*\.html)?$/);
+    if (!lemonde_match) throw new Error("Could not find keywords in lemonde url");
+    return lemonde_match[1];*/
+    throw new Error("not implemented");
+}
+
+async function createLink() {
+    const a = await ophirofoxEuropresseLink(extractKeywords());
+    a.classList.add("btn", "btn--premium");
+    return a;
+}
+
+async function onLoad() {
+    const article = document.querySelectorAll("article")[0];
+    const elt = article?.children[1].children[1].children[0].children[0].children[1].children[0]
+    if (elt) {
+    console.log("fig", elt);
+        elt.appendChild(await createLink());
+    }
+}
+
+onLoad().catch(console.error);
