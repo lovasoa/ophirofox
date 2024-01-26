19e98b57e4dfc84910b967d34f54cc077b4e0591
Write <541722+Write@users.noreply.github.com>
2024-01-26 23:43:56 +0000

    Corrige l'affichage du bouton sur leparisien.fr

--- ophirofox/content_scripts/le-parisien.js
+++ ophirofox/content_scripts/le-parisien.js
@@ -8,19 +8,46 @@ async function createLink() {
     return a;
 }
 
+async function addEuropresseButton() {
+    const head = document.querySelector("h1");
+    head.after(await createLink());
+}
 
 function findPremiumBanner() {
-    const title = document.querySelector(".paywall-sticky.width_full.d_flex.pt_3_m.pb_3_m.pt_4_nm.pb_4_nm.pos_stick.ff_gct.fw_r.justify_center");
-    if (!title) return null;
-    const elems = title.parentElement.querySelectorAll("div");
-    return [...elems].find(d => d.textContent.includes("Cet article est réservé aux abonnés"))
+
+    const isFirefox = chrome.runtime.getURL('').startsWith('moz-extension://');
+ 
+    if (isFirefox) {
+        const bannerSelectorString = 'paywall-sticky width_full d_flex pt_3_m pb_3_m pt_4_nm pb_4_nm pos_stick ff_gct fw_r justify_center';
+        const callback = (mutationList, observer) => {
+            for (const mutation of mutationList) {
+                for (const e of mutation.addedNodes) {
+                    if(e.className == bannerSelectorString) {
+                        observer.disconnect();
+                        const elems = e.parentElement.querySelectorAll("div");
+                        const matches = [...elems].find(d => d.textContent.includes("Cet article est réservé aux abonnés"));
+                        if (matches)
+                            addEuropresseButton();
+                    }
+                }
+            }
+        };
+        const observer = new MutationObserver(callback);
+        observer.observe(document.body, { childList: true });
+    } 
+    else {
+        const bannerSelector = document.querySelector(".paywall-sticky.width_full.d_flex.pt_3_m.pb_3_m.pt_4_nm.pb_4_nm.pos_stick.ff_gct.fw_r.justify_center");
+        if (!bannerSelector) return null;
+        const elems = bannerSelector.parentElement.querySelectorAll("div");
+        const matches = [...elems].find(d => d.textContent.includes("Cet article est réservé aux abonnés"));
+        if (matches)
+            addEuropresseButton();
+    }
+
 }
 
 async function onLoad() {
-	const premiumBanner = findPremiumBanner();
-    if (!premiumBanner) return;
-    const head = document.querySelector("h1");
-    head.after(await createLink());
+    findPremiumBanner();
 }
 
 onLoad().catch(console.error);
