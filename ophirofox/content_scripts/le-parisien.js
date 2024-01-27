f902974bda6cf442dfe47e3e347e1f0724f3aaa2
Write <541722+Write@users.noreply.github.com>
2024-01-27 01:37:27 +0000

    (LeParisien) Retire la vérification du navigateur et ajoute en fallback un MutationObserver

--- ophirofox/content_scripts/le-parisien.js
+++ ophirofox/content_scripts/le-parisien.js
@@ -13,47 +13,38 @@ async function addEuropresseButton() {
     head.after(await createLink());
 }
 
-function findPremiumBanner() {
+function findPremiumBanner(bannerSelector) {
+    if (!bannerSelector) return null;
+    const elems = bannerSelector.parentElement.querySelectorAll("div");
+    return [...elems].find(d => d.textContent.includes("Cet article est réservé aux abonnés"));
+}
 
-    const isFirefox = chrome.runtime.getURL('').startsWith('moz-extension://');
-
-    if (isFirefox) {
-        const bannerSelectorString = 'paywall-sticky width_full d_flex pt_3_m pb_3_m pt_4_nm pb_4_nm pos_stick ff_gct fw_r justify_center';
-        var elementFound = false;
+async function onLoad() {
+    const bannerSelector = document.querySelector(".paywall-sticky.width_full.d_flex.pt_3_m.pb_3_m.pt_4_nm.pb_4_nm.pos_stick.ff_gct.fw_r.justify_center");
+    if (findPremiumBanner(bannerSelector)) {
+        addEuropresseButton();
+    } 
+    else {
+        /* Premium banner couldn't be found, use MutationObserver as fallback */
         const callback = (mutationList, observer) => {
             for (const mutation of mutationList) {
                 for (const e of mutation.addedNodes) {
+                    const bannerSelectorString = 'paywall-sticky width_full d_flex pt_3_m pb_3_m pt_4_nm pb_4_nm pos_stick ff_gct fw_r justify_center';
+                    var elementFound = false;
                     if(e.className == bannerSelectorString) {
                         observer.disconnect();
                         elementFound = true;
-                        const elems = e.parentElement.querySelectorAll("div");
-                        const matches = [...elems].find(d => d.textContent.includes("Cet article est réservé aux abonnés"));
-                        if (matches)
+                        if(findPremiumBanner(e)) {
                             addEuropresseButton();
+                        }
                         break;
-                    }
-                }
-                if (elementFound) {
-                    break;
+                    }                    
                 }
             }
         };
         const observer = new MutationObserver(callback);
-        observer.observe(document.body, { childList: true });
-    } 
-    else {
-        const bannerSelector = document.querySelector(".paywall-sticky.width_full.d_flex.pt_3_m.pb_3_m.pt_4_nm.pb_4_nm.pos_stick.ff_gct.fw_r.justify_center");
-        if (!bannerSelector) return null;
-        const elems = bannerSelector.parentElement.querySelectorAll("div");
-        const matches = [...elems].find(d => d.textContent.includes("Cet article est réservé aux abonnés"));
-        if (matches)
-            addEuropresseButton();
+        observer.observe(document.body, { childList: true});
     }
-
-}
-
-async function onLoad() {
-    findPremiumBanner();
 }
 
 onLoad().catch(console.error);
