8cf2cb251f416272c11f8b205bd8bbb30eadf589
jeremie <91679287+JeremieGiffard@users.noreply.github.com>
2025-02-02 11:09:15 +0100

    BNF-only redirection mediapart 0.2

--- ophirofox/content_scripts/mediapart.js
+++ ophirofox/content_scripts/mediapart.js
@@ -1,60 +1,73 @@
-
 /**
  * @description create link <a> to BNF mirror
  * @param {string} AUTH_URL_MEDIAPART
  */
 async function createLink(AUTH_URL_MEDIAPART) {
-    const span = document.createElement("span");
-    span.textContent = "Lire avec BNF";
+  const span = document.createElement("span");
+  span.textContent = "Lire avec BNF";
 
-    const a = document.createElement("a");
-    a.href = new URL(window.location);
-    a.host = AUTH_URL_MEDIAPART
-    a.appendChild(span);
-
-    return a;
+  const a = document.createElement("a");
+  a.href = new URL(window.location);
+  a.host = AUTH_URL_MEDIAPART;
+  a.appendChild(span);
+  return a;
 }
 
 /**
- * @description check DOM for article under paywall 
+ * @description check DOM for article under paywall
  * @return {HTMLElement} DOM Premium Banner and head of the article
-*/
+ */
 function findPremiumBanner() {
-    const article = document.querySelector(".news__body__center__container");
-    if (!article) return null;
-    const elems = article.querySelectorAll(".paywall-message");
-    console.log("elements",elems)
-    //labels not the same for mobile or PC display
-    const textToFind = ["réservée aux abonné·es", "réservé aux abonné·es"]
+  const article = document.querySelector(".news__body__center__container");
+  if (!article) return null;
+  const elems = article.querySelectorAll(".paywall-message");
+  //labels not the same for mobile or PC display
+  const textToFind = ["réservée aux abonné·es", "réservé aux abonné·es"];
 
-    return [...elems].filter((balise) =>  textToFind.some((text) => balise.textContent.toLowerCase().includes(text))  )
-    
+  return [...elems].filter((balise) =>
+    textToFind.some((text) => balise.textContent.toLowerCase().includes(text))
+  );
 }
 
-/**@description check for BNF users. If yes, create link button */
+/**
+ * @description if not properly logged on the mirror website, fetch the login page
+ */
+function handleMediapartMirror(config) {
+  const navBar = document.querySelector("ul.nav__actions");
+  const spans = navBar.querySelectorAll("span");
+
+  let isNotConnected = Array.from(spans).find(
+    (elem) => elem.textContent == "Se connecter"
+  );
+  if (isNotConnected) {
+    //account name not found. fetch login page
+    const LOGIN_PAGE = new URL(
+      "licence",
+      "https://" + config.AUTH_URL_MEDIAPART
+    );
+    fetch(LOGIN_PAGE).then(() => window.location.reload());
+  }
+}
+
+async function handleMediapart(config) {
+  const reserve = findPremiumBanner();
+  if (!reserve) return;
+
+  for (const balise of reserve) {
+    balise.appendChild(await createLink(config.AUTH_URL_MEDIAPART));
+  }
+}
+
+/**@description check for users with mediapart access. If yes, create link button */
 async function onLoad() {
-
-    const config = await configurationsSpecifiques(['BNF'])
-    if(!config) return;
-    var newUrl = new URL(window.location);//current page
-    if (newUrl.host+'/' == 'www-mediapart-fr.bnf.idm.oclc.org/' ) {
-        const domNavigation = document.querySelector('ul.nav__actions')
-        const spans = domNavigation.querySelectorAll('span')
-        //search account name
-    let isFound =   Array.from(spans).find((elem) => elem.textContent == 'Bibliothèque Nationale de France')
-        if(!isFound){
-            console.log('is not found')
-            //account name not found. fetch login page
-            fetch("https://www-mediapart-fr.bnf.idm.oclc.org/licence").then(() => window.location.reload());
-        }
-    }else{
-        const reserve = findPremiumBanner();
-        if (!reserve) return;
-
-        for (const balise of reserve) {
-        balise.appendChild(await createLink(config.AUTH_URL_MEDIAPART))
-        }
-    }
+  const config = await configurationsSpecifiques(["BNF"]);
+  if (!config) return;
+  const currentPage = new URL(window.location);
+  if (currentPage.host == config.AUTH_URL_MEDIAPART) {
+    handleMediapartMirror(config);
+  } else {
+    handleMediapart(config);
+  }
 }
 
 onLoad().catch(console.error);
