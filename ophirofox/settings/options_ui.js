83b09ba94e50ece092153622b0bf36f15eade6b0
ferreol-fr <ponsich@laposte.net>
2025-06-03 16:45:13 +0200

    Ajout d'un menu recherche (nettoyage du code inutile)

--- ophirofox/settings/options_ui.js
+++ ophirofox/settings/options_ui.js
@@ -58,7 +58,7 @@ autoOpenLinkCheckbox.onchange = () => {
   setSettings(settings);
 };
 
-addSearchMenuCheckbox.onchange = async () => {
+addSearchMenuCheckbox.onchange = () => {
   settings.add_search_menu = addSearchMenuCheckbox.checked;
   setSettings(settings);
 };
@@ -79,77 +79,3 @@ search.oninput = () => {
     partner.hidden = !partner_name.includes(search_term);
   });
 };
-
-/*
-const permissionsToRequest = {
-  permissions: ["menus", "contextMenus","activeTab","storage","scripting","webRequest"],
-  origins: ["https://github.com/lovasoa/ophirofox"],
-};
-
-async function requestMenuPermission() {
-  alert('contextMenus');
-  function onResponse(response) {
-    if (response) {
-      alert("Permission was granted");
-    } else {
-      alert("Permission was refused");
-    }
-    return browser.permissions.getAll();
-  }
-
-  const response = await browser.permissions.request(permissionsToRequest);
-  const currentPermissions = await onResponse(response);
-
-  console.log(`Current permissions:`, currentPermissions);
-}
-
-document
-    .querySelector("#request")
-    .addEventListener("click", requestPermissions);
-
-
-/*
-function requestMenuPermission() {
-  alert('contextMenus');
-  browser.permissions.request({ permissions: ['menus'] }).then((granted) => {
-    if (granted) {
-      setupContextMenu();
-      alert("setupContextMenu");
-    } else {
-      alert("La permission a été refusée.");
-    }
-  });
-}
-
-function setupContextMenu() {
-  browser.contextMenus.create({
-    id: "EuropresseSearchMenu",
-    title: "Rechercher sur Europresse",
-    contexts: ["selection"]
-  });
-
-  function onCreated() {
-    if (browser.runtime.lastError) {
-      console.log(`Error: ${browser.runtime.lastError}`);
-    } else {
-      console.log("EuropresseSearchMenu created successfully");
-    }
-  }
-
-  browser.contextMenus.onClicked.addListener( async (info, tab) => {
-    switch (info.menuItemId) {
-      case "EuropresseSearchMenu":
-        console.log("EuropresseSearchMenu",info.selectionText);
-        const search_request = info.selectionText;
-        await chrome.storage.local.set({"EuropresseSearchMenu_request": search_request});
-        browser.tabs.create({
-          url: "https://bnf.idm.oclc.org/login?url=https://nouveau.europresse.com/access/ip/default.aspx?un=D000067U_1"
-        });
-        break;
-    }
-  });
-}
-
-// Appeler cette fonction en réponse à une action utilisateur, par exemple un clic sur un bouton
-document.getElementById('enable-context-menu').addEventListener('click', requestMenuPermission);
-*/
